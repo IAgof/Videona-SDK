cc714bc768fd0edd789be2df7db157e95dc880cb
alvaro martinez <a.martinez@videona.com>
2016-11-18 12:05:03 +0100

    - Update method to get real path from Uri.

--- app/src/main/java/com/videonasocialmedia/transcoder/TranscoderActivity.java
+++ app/src/main/java/com/videonasocialmedia/transcoder/TranscoderActivity.java
@@ -3,7 +3,10 @@ package com.videonasocialmedia.transcoder;
 import android.Manifest;
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
@@ -11,6 +14,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.os.SystemClock;
 import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -187,7 +191,7 @@ public class TranscoderActivity extends Activity implements OnAudioMixerListener
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         file = new File(externalDir, "transcode_test_" + timeStamp + ".mp4");
 
-        final String inPath = getPathFromUri(data.getData());
+        final String inPath = getPath(this, data.getData());
         final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progress_bar);
         progressBar.setMax(PROGRESS_BAR_MAX);
         final long startTime = SystemClock.uptimeMillis();
@@ -283,20 +287,139 @@ public class TranscoderActivity extends Activity implements OnAudioMixerListener
     }
   }
 
-  @TargetApi(Build.VERSION_CODES.KITKAT)
-  private String getPathFromUri(Uri uri) {
+  
+  /**
+   * Get a file path from a Uri. This will get the the path for Storage Access
+   * Framework Documents, as well as the _data field for the MediaStore and
+   * other file-based ContentProviders.
+   *
+   * @param context The context.
+   * @param uri The Uri to query.
+   * @author paulburke
+   */
+  public static String getPath(final Context context, final Uri uri) {
 
-    String wholeID = DocumentsContract.getDocumentId(uri);
+    final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
 
-    // Split at colon, use second item in the array
-    String id = wholeID.split(":")[1];
+    // DocumentProvider
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+      if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+        // ExternalStorageProvider
+        if (isExternalStorageDocument(uri)) {
+          final String docId = DocumentsContract.getDocumentId(uri);
+          final String[] split = docId.split(":");
+          final String type = split[0];
 
+          if ("primary".equalsIgnoreCase(type)) {
+            return Environment.getExternalStorageDirectory() + "/" + split[1];
+          }
 
-    String filePath = Environment.getExternalStorageDirectory() + File.separator + id;
+          // TODO handle non-primary volumes
+        }
+        // DownloadsProvider
+        else if (isDownloadsDocument(uri)) {
 
+          final String id = DocumentsContract.getDocumentId(uri);
+          final Uri contentUri = ContentUris.withAppendedId(
+              Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
 
-    return filePath;
+          return getDataColumn(context, contentUri, null, null);
+        }
+        // MediaProvider
+        else if (isMediaDocument(uri)) {
+          final String docId = DocumentsContract.getDocumentId(uri);
+          final String[] split = docId.split(":");
+          final String type = split[0];
+
+          Uri contentUri = null;
+          if ("image".equals(type)) {
+            contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+          } else if ("video".equals(type)) {
+            contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+          } else if ("audio".equals(type)) {
+            contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+          }
+
+          final String selection = "_id=?";
+          final String[] selectionArgs = new String[] {
+              split[1]
+          };
+
+          return getDataColumn(context, contentUri, selection, selectionArgs);
+        }
+      }
+      // MediaStore (and general)
+      else if ("content".equalsIgnoreCase(uri.getScheme())) {
+        return getDataColumn(context, uri, null, null);
+      }
+      // File
+      else if ("file".equalsIgnoreCase(uri.getScheme())) {
+        return uri.getPath();
+      }
+    }
+
+    return null;
   }
+
+  /**
+   * Get the value of the data column for this Uri. This is useful for
+   * MediaStore Uris, and other file-based ContentProviders.
+   *
+   * @param context The context.
+   * @param uri The Uri to query.
+   * @param selection (Optional) Filter used in the query.
+   * @param selectionArgs (Optional) Selection arguments used in the query.
+   * @return The value of the _data column, which is typically a file path.
+   */
+  public static String getDataColumn(Context context, Uri uri, String selection,
+                                     String[] selectionArgs) {
+
+    Cursor cursor = null;
+    final String column = "_data";
+    final String[] projection = {
+        column
+    };
+
+    try {
+      cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+          null);
+      if (cursor != null && cursor.moveToFirst()) {
+        final int column_index = cursor.getColumnIndexOrThrow(column);
+        return cursor.getString(column_index);
+      }
+    } finally {
+      if (cursor != null)
+        cursor.close();
+    }
+    return null;
+  }
+
+
+  /**
+   * @param uri The Uri to check.
+   * @return Whether the Uri authority is ExternalStorageProvider.
+   */
+  public static boolean isExternalStorageDocument(Uri uri) {
+    return "com.android.externalstorage.documents".equals(uri.getAuthority());
+  }
+
+  /**
+   * @param uri The Uri to check.
+   * @return Whether the Uri authority is DownloadsProvider.
+   */
+  public static boolean isDownloadsDocument(Uri uri) {
+    return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+  }
+
+  /**
+   * @param uri The Uri to check.
+   * @return Whether the Uri authority is MediaProvider.
+   */
+  public static boolean isMediaDocument(Uri uri) {
+    return "com.android.providers.media.documents".equals(uri.getAuthority());
+  }
+
+
 
   private void onTranscodeFinished(boolean isSuccess, String toastMessage) {
     final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progress_bar);
