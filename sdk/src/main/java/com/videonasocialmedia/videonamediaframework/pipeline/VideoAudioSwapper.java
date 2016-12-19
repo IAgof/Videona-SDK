c0a1b637b6f704fa391c6ea23cb7c626b95ead63
Javier Liarte <jliarte@gmail.com>
2016-12-19 15:45:51 +0100

    refactor and cleanup of VideoAudioSwapper

--- sdk/src/main/java/com/videonasocialmedia/videonamediaframework/pipeline/VideoAudioSwapper.java
+++ sdk/src/main/java/com/videonasocialmedia/videonamediaframework/pipeline/VideoAudioSwapper.java
@@ -1,11 +1,13 @@
 package com.videonasocialmedia.videonamediaframework.pipeline;
 
+import android.support.annotation.NonNull;
 import android.util.Log;
 
 import com.googlecode.mp4parser.authoring.Movie;
 import com.googlecode.mp4parser.authoring.Track;
 import com.googlecode.mp4parser.authoring.container.mp4.MovieCreator;
 import com.googlecode.mp4parser.authoring.tracks.AppendTrack;
+import com.videonasocialmedia.videonamediaframework.muxer.utils.Utils;
 
 import java.io.File;
 import java.io.IOException;
@@ -58,30 +60,39 @@ public class VideoAudioSwapper implements ExporterVideoSwapAudio {
     return result;
   }
 
-  private Movie swapAudio(Movie movie, String audioPath, double movieDuration) throws IOException {
+  private Movie swapAudio(Movie originalMovie, String audioPath, double movieDuration)
+          throws IOException {
     Movie finalMovie = new Movie();
-
-    List<Track> videoTrack = new LinkedList<>();
-
-    for (Track t : movie.getTracks()) {
-      if (t.getHandler().equals("vide")) {
-        videoTrack.add(t);
-      }
-    }
+    List<Track> videoTrack = extractVideoTracks(originalMovie);
+    finalMovie.addTrack(new AppendTrack(videoTrack.toArray(new Track[videoTrack.size()])));
 
     Movie audioMovie = MovieCreator.build(audioPath);
-    List<Track> audioTracks = new LinkedList<>();
-
-    for (Track t : audioMovie.getTracks()) {
-      if (t.getHandler().equals("soun")) {
-        audioTracks.add(t);
-      }
-    }
-
-    finalMovie.addTrack(new AppendTrack(videoTrack.toArray(new Track[videoTrack.size()])));
+    List<Track> audioTracks = extractAudioTracks(audioMovie);
     finalMovie.addTrack(new AppendTrack(audioTracks.toArray(new Track[audioTracks.size()])));
 
     return finalMovie;
+  }
+
+  @NonNull
+  private List<Track> extractAudioTracks(Movie audioMovie) {
+    return extractTracks(audioMovie, "soun");
+  }
+
+  @NonNull
+  private List<Track> extractVideoTracks(Movie originalMovie) {
+    return extractTracks(originalMovie, "vide");
+  }
+
+  @NonNull
+  private List<Track> extractTracks(Movie movie, String trackType) {
+    List<Track> videoTrack = new LinkedList<>();
+
+    for (Track t : movie.getTracks()) {
+      if (t.getHandler().equals(trackType)) {
+        videoTrack.add(t);
+      }
+    }
+    return videoTrack;
   }
 
   private double getMovieDuration(Movie movie) {
@@ -94,7 +105,7 @@ public class VideoAudioSwapper implements ExporterVideoSwapAudio {
   private void saveFinalVideo(Movie result, String outputFilePath) {
     try {
       long start = System.currentTimeMillis();
-      com.videonasocialmedia.videonamediaframework.muxer.utils.Utils.createFile(result, outputFilePath);
+      Utils.createFile(result, outputFilePath);
       long spent = System.currentTimeMillis() - start;
       Log.d("WRITING VIDEO FILE", "time spent in millis: " + spent);
       videoAudioSwapperListener.onExportSuccess();
