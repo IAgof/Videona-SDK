1dd1d602b16b371ad277c778ee3603f36c53ea0f
Javier Liarte <jliarte@gmail.com>
2017-04-10 16:13:30 +0200

    Recovered OnAudioEncoderListener.OnFileDecodedSuccess deleted by error

--- sdk/src/main/java/com/videonasocialmedia/transcoder/audio/listener/OnAudioEncoderListener.java
+++ sdk/src/main/java/com/videonasocialmedia/transcoder/audio/listener/OnAudioEncoderListener.java
@@ -4,7 +4,7 @@ package com.videonasocialmedia.transcoder.audio.listener;
  * Created by alvaro on 19/09/16.
  */
 public interface OnAudioEncoderListener {
-
     void OnFileEncodedSuccess(String outputFile);
     void OnFileEncodedError(String error);
+    void OnFileDecodedSuccess(String outputFile);
 }
