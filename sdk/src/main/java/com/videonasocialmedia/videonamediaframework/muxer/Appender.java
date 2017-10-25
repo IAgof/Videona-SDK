9ee8200878624b75ea0d3c49ccb0f7f7d1d69b70
Alvaro <a.martinez@videona.com>
2017-10-25 11:59:29 +0200

    - Appender. Update appendVideos method. Remove boolean addOriginalAudio not used. Added CroppedTrack to audio, advance some samples/milliseconds audio over video, trying to "resolve/unmask" synchronization problems between audio and video.

--- sdk/src/main/java/com/videonasocialmedia/videonamediaframework/muxer/Appender.java
+++ sdk/src/main/java/com/videonasocialmedia/videonamediaframework/muxer/Appender.java
@@ -6,6 +6,7 @@ import com.googlecode.mp4parser.authoring.Movie;
 import com.googlecode.mp4parser.authoring.Track;
 import com.googlecode.mp4parser.authoring.container.mp4.MovieCreator;
 import com.googlecode.mp4parser.authoring.tracks.AppendTrack;
+import com.googlecode.mp4parser.authoring.tracks.CroppedTrack;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -19,21 +20,22 @@ import java.util.List;
 public class Appender {
     private static final String LOG_TAG = Appender.class.getCanonicalName();
 
-    public Movie appendVideos(List<String> videoPaths, boolean addOriginalAudio)
+    public Movie appendVideos(List<String> videoPaths)
             throws IOException, IntermediateFileException {
         List<Movie> movieList = getMovieList(videoPaths);
         List<Track> videoTracks = new LinkedList<>();
         List<Track> audioTracks = new LinkedList<>();
 
         for (Movie m : movieList) {
-            for (Track t : m.getTracks()) {
-                if(addOriginalAudio) {
-                    if (t.getHandler().equals("soun")) {
-                        audioTracks.add(t);
-                    }
+            for (Track track : m.getTracks()) {
+                if (track.getHandler().equals("soun")) {
+                    // removes the first samples and shortens the AAC track by ~22ms per sample
+                    CroppedTrack audioTrackShort = new CroppedTrack(track, 5,
+                            track.getSamples().size());
+                    audioTracks.add(audioTrackShort);
                 }
-                if (t.getHandler().equals("vide")) {
-                    videoTracks.add(t);
+                if (track.getHandler().equals("vide")) {
+                    videoTracks.add(track);
                 }
             }
         }
